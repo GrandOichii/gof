856da61fa9fb22d3a9da81f39f020a2c84243c5d
Igor Yastrebov <i.hawk@yandex.ru>
2024-08-17 18:04:13 +0300

    Behavioral: add example for Momento

--- Behavioral/Momento/Program.cs
+++ Behavioral/Momento/Program.cs
@@ -1,7 +1,5 @@
 ﻿namespace Momento;
 
-// TODO add concrete
-
 #region Abstract
 
 public class Caretaker {
@@ -33,9 +31,78 @@ public class Originator {
 
 #endregion
 
+#region Concrete
+
+// Momento
+public class Document {
+    private List<string> _pages = [];
+    
+    public Document(List<string> pages) {
+        _pages = pages;
+    }
+
+    public List<string> GetPages() { return _pages; }
+    public void SetPages(List<string> pages) { _pages = pages; }
+}
+
+// Caretaker
+public class DocumentHistory {
+    public List<Document> History { get; } = [];
+
+    public void Add(Document document) { History.Add(document); }
+}
+
+public class Editor {
+    public List<string> Pages { get; private set; } = [];
+
+    public void SetDocument(Document doc) {
+        Pages = doc.GetPages();
+    }
+
+    public Document CreateDocument() {
+        return new Document(new(Pages));
+    }
+
+    public void Add(int pageI, string text) {
+        while (Pages.Count <= pageI) {
+            Pages.Add("");
+        }
+
+        Pages[pageI] += text;
+    }
+
+    public void PrintContents() {
+        for (int i = 0; i < Pages.Count; i++) {
+            Console.WriteLine($"[Page {i + 1}]");
+            Console.WriteLine(Pages[i]);
+            Console.WriteLine();
+        }
+    }
+}
+
+#endregion
+
 public class Program {
 
     public static void Main(string[] args) {
+        var history = new DocumentHistory();
+        var editor = new Editor();
 
+        editor.Add(0, "Hello");
+        editor.Add(1, "This is an example of a momento");
+        history.Add(editor.CreateDocument());
+
+        editor.Add(0, ", world!");
+        history.Add(editor.CreateDocument());
+
+        var momento1 = history.History[0];
+        editor.SetDocument(momento1);
+        System.Console.WriteLine("Momento 1");
+        editor.PrintContents();
+
+        var momento2 = history.History[1];        
+        editor.SetDocument(momento2);
+        System.Console.WriteLine("Momento 2");
+        editor.PrintContents();
     }
 }
