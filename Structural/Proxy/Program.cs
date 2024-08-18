71aa6734ba75486dd4c11fd5718e4415d95837bf
Igor Yastrebov <i.hawk@yandex.ru>
2024-08-18 11:21:59 +0300

    Structural: add example for Proxy

--- Structural/Proxy/Program.cs
+++ Structural/Proxy/Program.cs
@@ -1,7 +1,5 @@
 ﻿namespace Proxy;
 
-// TODO add concrete
-
 #region Abstract
 
 public interface ISubject {
@@ -27,9 +25,51 @@ public class Proxy : ISubject {
 
 #endregion
 
+#region Concrete
+
+public interface IImage {
+    public byte[] GetImage();
+}
+
+public class FileImage : IImage {
+    private byte[] _content;
+
+    public FileImage(string path) {
+        System.Console.WriteLine($"Reading image from file {path}");
+        _content = [];
+    }
+
+    public byte[] GetImage() {
+        return _content;
+    }
+}
+
+public class FileImageProxy : IImage {
+    private FileImage? _wrapped;
+    private string _path;
+
+    public FileImageProxy(string path) {
+        _path = path;
+        _wrapped = null;
+    }
+
+    public byte[] GetImage() {
+        if (_wrapped is null)
+            _wrapped = new FileImage(_path);
+
+        return _wrapped.GetImage();
+    }
+}
+
+#endregion
+
 public class Program {
 
     public static void Main(string[] args) {
+        IImage image1 = new FileImageProxy("images/cat.png");
+        IImage image2 = new FileImageProxy("images/dog.png");
 
+        System.Console.WriteLine(image1.GetImage());
+        System.Console.WriteLine(image2.GetImage());
     }
 }
