af0951be3ae2de1d9865569ce0a96bcc603ff90f
Igor Yastrebov <i.hawk@yandex.ru>
2024-08-18 11:39:55 +0300

    Behavioral: add example for state

--- Behavioral/State/Program.cs
+++ Behavioral/State/Program.cs
@@ -1,6 +1,6 @@
-﻿namespace State;
+﻿using Microsoft.VisualBasic;
 
-// TODO add concrete
+namespace State;
 
 #region Abstract
 
@@ -32,9 +32,89 @@ public class ConcreteStateB : IState {
 
 #endregion
 
+#region Concrete
+
+public class Creature {
+    public ICreatureState State { get; set; }
+    public string Name { get; }
+
+    public Creature(string name, ICreatureState initialState) {
+        Name = name;
+        State = initialState;
+    }
+
+    public void Behave() {
+        State.Handle(this);
+    }
+}
+
+public interface ICreatureState {
+    public void Handle(Creature creature);
+}
+
+public class Wonder : ICreatureState
+{
+    private static readonly Random Rng = new();
+
+    public ICreatureState? SeePlayerReaction { get; set; } = null;
+
+    private bool SeesPlayer() {
+        return Rng.Next() % 3 == 0;
+    }
+
+    public void Handle(Creature creature)
+    {
+        System.Console.WriteLine($"{creature.Name} wonders");
+
+        if (SeesPlayer() && SeePlayerReaction is not null) {
+            System.Console.WriteLine($"{creature.Name} noticed the player");
+            creature.State = SeePlayerReaction;
+        }
+    }
+}
+
+public class Chase : ICreatureState
+{
+    private static readonly Random Rng = new();
+
+    public Wonder Wonder { get; }
+
+    public Chase(Wonder wonder) {
+        Wonder = wonder;
+    }
+
+    private bool LosesInterest() {
+        return Rng.Next() % 2 == 0;
+    }
+
+    public void Handle(Creature creature)
+    {
+        System.Console.WriteLine($"{creature.Name} chases after the player");
+
+        if (LosesInterest()) {
+            System.Console.WriteLine($"{creature.Name} loses interest");
+            creature.State = Wonder;
+        }
+    }
+
+}
+
+#endregion
+
 public class Program {
 
     public static void Main(string[] args) {
+        var wonder = new Wonder();
 
+        var chase = new Chase(wonder);
+        wonder.SeePlayerReaction = chase;
+
+        var creature = new Creature("Zombie", wonder);
+
+        while (true) {
+            creature.Behave();
+
+            Thread.Sleep(500);
+        }
     }
 }
